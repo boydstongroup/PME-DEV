881a45ad8261a09e17a56f3c6fb01d9012037c28
CoCl-Jerry <101297585+boydstongroup@users.noreply.github.com>
2022-05-22 15:50:12 -0500

    Update Temp.py

--- _python/Temp.py
+++ _python/Temp.py
@@ -6,7 +6,7 @@ cpu = CPUTemperature()
 
 def write_temp(temp):
     with open("/home/pi/cpu_temp.csv", "a") as log:
-        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(temp)))
+        log.write(str(temp))
 
 
 while True:
