60022fc563d0c1595f12625819b1d4b2e50200a9
CoCl-Jerry <101297585+boydstongroup@users.noreply.github.com>
2022-05-22 22:33:00 -0500

    Update UI_Update.py

--- _python/UI_Update.py
+++ _python/UI_Update.py
@@ -66,7 +66,7 @@ def gradient_complete(self):
     self.ex1_lcdNumber.display(Settings.ex2_CurrentInterval)
 
 
-def interval_update(self):
+def gradient_update(self):
     self.ex1_lcdNumber.display(Settings.ex1_CurrentInterval)
     self.ex1_lcdNumber.display(Settings.ex2_CurrentInterval)
 
